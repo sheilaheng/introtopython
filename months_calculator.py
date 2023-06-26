b2ca7e2bb1400edea5342d3ad1756d5def79c662
dowido <kwambokasheilah45@gmail.com>
2023-06-26 08:45:00 +0300

    added months calculator file

---
+++ months_calculator.py
@@ -0,0 +1,6 @@
+user_age = input("Enter your age")
+age = int(user_age )
+
+calc_months = age * 12
+
+print(calc_months)
---
+++ userinput.py
@@ -0,0 +1,4 @@
+name = input("what is your name")
+age = input("what is your age")
+print(name)
+print(age)
