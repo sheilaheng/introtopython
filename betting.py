317cc404ab4068d3afc5e273f2c2f69a9a41f4ef
dowido <kwambokasheilah45@gmail.com>
2023-06-27 10:10:47 +0300

    conditional statement

---
+++ betting.py
@@ -0,0 +1,14 @@
+number = int(input("Enter any number between 1 to 5"))
+
+if number == 1:
+    print("You have won a BMW X5")
+elif number == 2:
+    print("You have won KSH50,000")
+elif number == 3:
+    print("You have won a free trip")
+elif number == 4:
+    print("you have won a free shopping spree")
+elif number == 5:
+    print("You have won a house")
+else:
+    print("Invalid number entered")
