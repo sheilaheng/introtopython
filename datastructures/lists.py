a17b3d3963333208e4d1a525bb534bf8f34d0453
dowido <kwambokasheilah45@gmail.com>
2023-06-29 10:29:02 +0300

    lists

--- datastructures/lists.py
+++ datastructures/lists.py
@@ -1,5 +1,9 @@
-friends = ["Kwamboka", "Sheilah","Barbara"]
+friends = ["Bob","Kwamboka", "Sheilah","Barbara","Ann","Kim"]
 friends.append("Gill")
 friends.append("Dakte")
 friends.remove("Dakte")
-print(friends)
+
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
---
+++ datastructures/tuples.py
@@ -0,0 +1,7 @@
+#defined by normal brackets
+#you cant modify a tuple
+#it keeps the order
+
+myteam = ("Sheilah","Angie","Joe")
+
+print(myteam)
