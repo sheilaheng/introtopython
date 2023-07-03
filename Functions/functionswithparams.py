399d00be1ab3a2affb1619736f38dd86c7b36a98
dowido <kwambokasheilah45@gmail.com>
2023-07-03 10:29:03 +0300

    functions with params

---
+++ Functions/functionswithparams.py
@@ -0,0 +1,10 @@
+def addnums(x, y):
+    result = x + y
+    print(result)
+
+addnums(20, 70)
+
+
+def showname(first, last):
+    print(first, last)
+showname(input("Enter First Name"), input("Enter Last Name"))
