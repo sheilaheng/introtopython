bc674bb56726b24ae103ea0c0e850ae9ace26390
dowido <kwambokasheilah45@gmail.com>
2023-07-03 09:10:50 +0300

    first commit

---
+++ loops/whileloopex.py
@@ -0,0 +1,9 @@
+#using rane fron 1 - 1000
+#print all numbers using while loop
+#ensure user inputs number in the terminal
+
+x = int(input("Enter first number"))
+m = int(input("Enter second number"))
+while x <= m:
+    print(x)
+    x +=1
