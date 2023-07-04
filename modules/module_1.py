94c77f353aadaed7cb9ee5b3b0e28dc9b5b80d34
dowido <kwambokasheilah45@gmail.com>
2023-07-04 09:01:24 +0300

    module files

---
+++ modules/module_1.py
@@ -0,0 +1,8 @@
+def addtwonums (num1,num2):
+    return num1 + num2
+
+def multtwonums (num1, num2):
+    return num1 * num2
+
+def subtwonums (num1, num2):
+    return num1 - num2
---
+++ modules/module_2.py
@@ -0,0 +1,5 @@
+from module_1 import *
+
+print(addtwonums(15, 75))
+print(multtwonums(100, 4))
+print(subtwonums(500, 350))
