cbe3034218eb2ac0982c7f768f6fb1a051569323
dowido <kwambokasheilah45@gmail.com>
2023-07-04 10:21:39 +0300

    summary

---
+++ OOP/summaryoop.py
@@ -0,0 +1,9 @@
+#oop is a paradigm / a way of wrighting code that makes it more reusable
+#we have classes and objects
+#a class is like a blueprint
+#objects - instance of a class{create as many as u like}
+
+#Inharitance
+#Polymorphysim
+#Abstraction
+#Encapsulation
